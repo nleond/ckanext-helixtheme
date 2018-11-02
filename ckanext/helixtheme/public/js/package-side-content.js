a390398fefa77c4f24e46315332c5acd3168d9dd
nleond <nikleonidakis@gmail.com>
2018-11-02 18:30:17 +0200

    New dataset creation

--- ckanext/helixtheme/public/js/package-side-content.js
+++ ckanext/helixtheme/public/js/package-side-content.js
@@ -12,7 +12,7 @@ function handleSideContent() {
 
     function init() {
         // turn to uppercase and remove greek accent 
-        var sections = $('.results-main-sidebar .side-content');
+        var sections = $('.results-main-sidebar .side-tags');
         var list = sections.find('a');
         list.each(function (index) {$(this).text($(this).text().toUpperCase().replace(/Ά/g, 'Α').replace(/Έ/g, 'Ε').replace(/Ή/g, 'Η').replace(/Ί/g, 'Ι').replace( /Ό/g, 'Ο').replace(/Ύ/g, 'Υ').replace(/Ώ/g, 'Ω'));
         });
